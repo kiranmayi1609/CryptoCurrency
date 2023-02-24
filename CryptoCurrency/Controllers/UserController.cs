27d5fb6235e31fac7940a13a7a32f32a7e16a864
Kiranmayi Tummala <kira1995@elev.tec.dk>
2023-02-24 12:34:28 +0100

    d 24.02

---
+++ CryptoCurrency/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using CryptoCurrency.Dto;
+using CryptoCurrency.Interfaces;
+using CryptoCurrency.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoCurrency.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUser _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUser userservice, IMapper mapper)
+        {
+
+            _userService = userservice;
+            _mapper = mapper;
+        }
+
+        //[HttpPost("authenticate")]
+        //public IActionResult Authenticate([FromBody] UserDto userobj)
+        //{
+        //    if (userobj == null)
+        //        return BadRequest();
+
+        //    // Find the user by email and password
+        //    var user = _userService.Authenticate(userobj.Email, userobj.Password);
+
+        //    if (user == null)
+        //        return NotFound(new { Message = "User not found" });
+
+
+        //    // Return the token to the client
+        //    return Ok(new
+        //    {
+        //        Message = "Login Success",
+
+        //    });
+        //}
+
+
+        //[HttpPost("registration")]
+        //    public IActionResult Register([FromBody] UserDto userobj)
+        //    {
+        //        if (userobj == null)
+        //        {
+        //            return BadRequest(new { message = "User object is null" });
+        //        }
+
+        //        if (_userService.UserExists(userobj.Id))
+        //        {
+        //            return Conflict(new { message = "User with this ID already exists" });
+        //        }
+        //        var user=_mapper.Map<User>(userobj);
+
+        //        if (_userService.CreateUser(user))
+        //        {
+        //            return Ok(new { message = "User created successfully" });
+        //        }
+        //        else
+        //        {
+        //            return BadRequest(new { message = "Failed to create user" });
+        //        }
+        //    }
+
+
+        [HttpPost("Registration")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateUser([FromBody] UserDto userobj)
+        {
+            if (userobj == null)
+                return BadRequest(ModelState);
+
+            var existingUser = _userService.GetUsers()
+                .Where(x => x.Id == userobj.Id).FirstOrDefault();
+
+            if (existingUser != null)
+            {
+                ModelState.AddModelError("", " user with this first name already exists ");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var UserMap = _mapper.Map<User>(userobj);
+
+            if (!_userService.CreateUser(UserMap))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok("Successfully created");
+        }
+    }
+}
--- CryptoCurrency/Models/User.cs
+++ CryptoCurrency/Models/User.cs
@@ -6,7 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Username { get; set; }
+        //public string Username { get; set; }
          public string Password { get; set; }
 
         //one to many relationship with Transaction 
